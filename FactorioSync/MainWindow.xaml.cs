da04e32c9c96be055d672ee2e65a9c0442833c68
zac@zacland.net <Manitoba#34>
2020-02-24 10:04:26 +0100

    Version 1.0 ok

--- FactorioSync/MainWindow.xaml.cs
+++ FactorioSync/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace FactorioSync
 {
@@ -28,26 +15,8 @@ namespace FactorioSync
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            string[] originalFiles = Directory.GetFiles(TxtSource.Text, "*", SearchOption.AllDirectories);
-
-            Array.ForEach(originalFiles, (originalFileLocation) =>
-            {
-                FileInfo originalFile = new FileInfo(originalFileLocation);
-                FileInfo destFile = new FileInfo(originalFileLocation.Replace(TxtSource.Text, TxtDestination.Text));
-
-                if (destFile.Exists)
-                {
-                    if (originalFile.Length > destFile.Length)
-                    {
-                        originalFile.CopyTo(destFile.FullName, true);
-                    }
-                }
-                else
-                {
-                    Directory.CreateDirectory(destFile.DirectoryName);
-                    originalFile.CopyTo(destFile.FullName, false);
-                }
-            });
+            ZacFunctions.CopyFile(TxtSource.Text.ToString(), TxtDestination.Text.ToString());
+            ZacFunctions.CopyFile(TxtDestination.Text.ToString(), TxtSource.Text.ToString());
         }
     }
 }
---
+++ FactorioSync/ZacFunctions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using NLog;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioSync
+{
+    class ZacFunctions
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void CopyFile(string Src_FOLDER, string Dest_FOLDER)
+        {
+            string[] originalFiles = Directory.GetFiles(Src_FOLDER, "*", SearchOption.AllDirectories);
+
+            logger.Debug("Début : {}", DateTime.Now);
+            logger.Debug("Il y a {} fichiers dans {}", originalFiles.Length, Src_FOLDER);
+
+            Array.ForEach(originalFiles, (originalFileLocation) =>
+            {
+                try
+                {
+                    FileInfo originalFile = new FileInfo(originalFileLocation);
+                    FileInfo destFile = new FileInfo(originalFileLocation.Replace(Src_FOLDER, Dest_FOLDER));
+
+                    logger.Debug("Copie du fichier {} vers le répertoire {}", originalFile, Dest_FOLDER);
+
+                    if (destFile.Exists)
+                    {
+                        logger.Debug("Le fichier {} existe déja !", destFile);
+                        if (originalFile.Length > destFile.Length)
+                        {
+                            logger.Debug("Le fichier d'origine \"{}\" est plus récent que le fichier de destination \"{}\" : On l'écrase", originalFile, destFile);
+                            originalFile.CopyTo(destFile.FullName, true);
+                        }
+                    }
+                    else
+                    {
+                        logger.Debug("Création/Sélection du répertoire \"{}\"", destFile.DirectoryName);
+                        Directory.CreateDirectory(destFile.DirectoryName);
+                        logger.Debug("Copie du fichier {}", destFile.FullName);
+                        originalFile.CopyTo(destFile.FullName, false);
+                    }
+                }
+                catch (Exception e)
+                {
+                    logger.Error(e);
+                }
+            });
+
+            logger.Debug("Fin : {}", DateTime.Now);
+            logger.Debug("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+        }
+    }
+}
