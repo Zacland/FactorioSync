fe3a1b74fdc410dc9a82eeb9dc08b762772a881b
Zac <zac@zacland.net>
2020-03-02 09:58:00 +0100

    [add] ligne de commande /silent

--- FactorioSync/MainWindow.xaml.cs
+++ FactorioSync/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System;
 using System.Drawing;
 using System.Windows.Media;
 
@@ -9,6 +10,8 @@ namespace FactorioSync
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string[]  args = Environment.GetCommandLineArgs();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,11 +22,25 @@ namespace FactorioSync
             BtnReset_Click(sender, e);
             ZacFunctions.CopyFile(TxtSource.Text.ToString(), TxtDestination.Text.ToString(), listBox);
             ZacFunctions.CopyFile(TxtDestination.Text.ToString(), TxtSource.Text.ToString(), listBox);
+
+
+            if ((args.Length>1) && args[1] == "/silent")
+            {
+                this.Close();
+            }
         }
 
         private void BtnReset_Click(object sender, RoutedEventArgs e)
         {
             listBox.Items.Clear();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if ((args.Length>1) && args[1] == "/silent")
+            {
+                button_Click(sender, e);
+            }
         }
     }
 }
